faaacd2e5db3dd8f2a5c486cdd1a44496c458dc4
Tunde Ayodele <tayodele@efficion.org>
2021-01-28 21:07:46 +0100

    complete front end

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+import { Switch, Text, TextInput } from "react-native";
+import ListingEditScreen from "./app/screens/ListingEditScreen"
+import Screen from "./app/components/Screen"
+  
+export default function App() {
+  return (
+     <Screen>
+
+     </Screen>
+  );
+}
